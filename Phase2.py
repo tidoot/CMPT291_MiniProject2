9eab95b077e121b1c69b0fa76674f0d2c861e148
JohnYujj <35233051+JohnYujj@users.noreply.github.com>
2019-11-21 12:39:55 -0700

    subprocesses sort, parse file removing backslash and splitting data/key

--- Phase2.py
+++ Phase2.py
@@ -1,12 +1,13 @@
 from bsddb3 import db
+import subprocess
 
 def main():
+    '''
     DB_File = "data.db"
     database = db.DB()
     database.set_flags(db.DB_DUP) #declare duplicates allowed before you create the database
     database.open(DB_File,None, db.DB_HASH, db.DB_CREATE)
     curs = database.cursor()
-
 
     def recsIndex():
         pass
@@ -20,10 +21,41 @@ def main():
     def datesIndex():
         pass
     
-    
-    
     curs.close()
     database.close()
+    '''
+    sortFiles()
+    parseFiles()
+
+def sortFiles():
+    subprocess.call(['sort', '-u', '-o', 'terms.txt', 'terms.txt'])  
+    subprocess.call(['sort', '-u', '-o', 'recs.txt', 'recs.txt'])  
+    subprocess.call(['sort', '-u', '-o', 'emails.txt', 'emails.txt'])  
+    subprocess.call(['sort', '-u', '-o', 'dates.txt', 'dates.txt'])  
+
+def parseFiles():
+    termsDataList, termsKeyList = parseFile('terms.txt')
+    emailsDataList, emailsKeyList = parseFile('emails.txt')
+    datesDataList, datesKeyList = parseFile('dates.txt')
+    recsKeyList, recsDataList = parseFile('recs.txt') #recs is reversed key,data
+
+def parseFile(file):
+    f = open(file,'r')
+    dataList = []
+    keyList = []
+    lines = f.read().splitlines()
+    for line in lines:
+        data,key = line.split(':', 1) #split at first occurance of semicolon only, applicable for recs
+        key = key.replace('\\','') #remove backslash
+        data = data.replace('\\','')
+        dataList.append(data)
+        keyList.append(keys)
+    f.close()
+    return dataList, keyList
+
+def loadDB(file):
+    pass
+    #subprocess.call(['db_load', '-c', 'duplicates', '-t', 'Btree', '-f']) 
 
 if __name__ == '__main__':
     main()
