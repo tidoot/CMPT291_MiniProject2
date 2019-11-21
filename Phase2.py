dec89eb10aa22cb3cc938a0cd0606362c7ee3317
tinado121 <56741509+tinado121@users.noreply.github.com>
2019-11-21 08:26:54 -0700

    Added def template to Phase 2

--- Phase2.py
+++ Phase2.py
@@ -1,19 +1,29 @@
 from bsddb3 import db
-DB_File = "data.db"
-database = db.DB()
-database.set_flags(db.DB_DUP) #declare duplicates allowed before you create the database
-database.open(DB_File,None, db.DB_HASH, db.DB_CREATE)
-curs = database.cursor()
 
-#Insert key-values including duplicates …
-database.put(b'key1', "value1")
-database.put(b'key1', "value2")
-database.put(b'key2', "value1")
-database.put(b'key2', "value2")
+def main():
+    DB_File = "data.db"
+    database = db.DB()
+    database.set_flags(db.DB_DUP) #declare duplicates allowed before you create the database
+    database.open(DB_File,None, db.DB_HASH, db.DB_CREATE)
+    curs = database.cursor()
 
 
+    def recsIndex():
+        pass
+    
+    def termsIndex():
+        pass
+    
+    def emailsIndex():
+        pass
+    
+    def datesIndex():
+        pass
+    
+    
+    
+    curs.close()
+    database.close()
 
-
-
-curs.close()
-database.close()
+if __name__ == '__main__':
+    main()
