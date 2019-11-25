fd1b909dbd8f6dbaa2ac4e2e9e43d4993f5bb779
Marshal Cheng <marshal1@cs.ualberta.ca>
2019-11-25 12:05:46 -0700

    return

--- Phase3.py
+++ Phase3.py
@@ -1,5 +1,6 @@
 from bsddb3 import db
 import subprocess
+import re
 
 def main():
     quit = False
@@ -44,29 +45,12 @@ def main():
             #Prints full record
             return record  
         
-def rangeSearch(start, end):
-    idxLst = []
-    result = curs.set_range(start.encode("utf-8"))
-    while(result != None):
-        #Checking the end condition: If the student's name comes after(or equal to) Ending_Name
-        if(str(result[0].decode("utf-8")[0:len(Ending_Name)])>=Ending_Name): 
-            break
-            
-        print("Name: " + str(result[0].decode("utf-8")) + ", Mark: " + str(result[1].decode("utf-8")))
-        idxList.append(result[0])
-        result = curs.next()
-        
 def getSubject():
-    database = db.DB()
-    curs = database.cursor()
-    database.open(idx)         
-    subStart = input("Enter Start Subject")
-    subEnd = input("Enter End Subject, enter nothing for no end")
-    idxLst = rangeSearch(subStart, subEnd)
-    print(idxLst)
-    #valueLst = fileGet(idxLst)
-    curs.close()
-    database.close()
+    filetext = 'rtest.txt'.readall()
+    subject = input("Enter Subject")
+    pattern = "<subj>" + subject
+    re.findall(pattern, filetext)
+    
 
 
 if __name__ == '__main__':
