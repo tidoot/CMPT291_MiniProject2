74eff45b52c5561866c982cb70865c304a43966f
tinado121 <56741509+tinado121@users.noreply.github.com>
2019-11-25 00:15:54 -0700

    Update Phase3.py

--- Phase3.py
+++ Phase3.py
@@ -2,16 +2,47 @@ from bsddb3 import db
 import subprocess
 
 def main():
-
-    DB_File = "data.db"
-    database = db.DB()
-    database.set_flags(db.DB_DUP) #declare duplicates allowed before you create the database
-    database.open(DB_File,None, db.DB_HASH, db.DB_CREATE)
-    curs = database.cursor()
-
+    quit = False
+    outputFull = False
     
-    curs.close()
-    database.close()
+    while not quit:
+        answer = input('Type "output=full" to view the full record. \nType "output=brief" to return to default view. \nType "q" to quit. \nPlease enter your queries: ')
+        if answer == 'output=full':
+            outputFull = True
+            print('-------------\nOutput has been changed to full view.\n')
+        elif answer == 'output=brief':
+            outputFull = False
+            print('-------------\nOutput has been changed to briew view.\n')
+        elif answer == 'q':
+            quit=True
+        else:
+            record = processQuery(answer)
+            print(record)
+
+        
+    
+    def processQuery(answer):
+        # NEED TO WORK ON THIS PART
+    #Processes string to pass to getRecords
+        getRecords(idx,answer)
+       
+        
+    def getRecords(idx,answer):
+        # NEED TO WORK ON THIS PART
+    #Accesses the database and retursn the record
+        database = db.DB()
+        curs = database.cursor()
+        database.open(idx)        
+        curs.close()
+        database.close() 
+        
+        # NEED TO WORK ON THIS PART
+        if outputFull == False:
+            #Prints record in brief view
+            return record
+        elif outputFull == True:
+            #Prints full record
+            return record      
 
 
 if __name__ == '__main__':
