11682b0866c2ac3cff91af8f1cd78c102778200f
9614 <46434108+9614@users.noreply.github.com>
2019-11-27 15:45:32 -0700

    Update Phase1.py

--- Phase1.py
+++ Phase1.py
@@ -54,7 +54,6 @@ def main():
     time_interval = end-start
     print("Time Completed: " + str(time_interval))
 
-
 def replaceSpecial(line):
     # Replaces any special characters used in termsTxt
     line = line.replace('&lt;', '<')
@@ -64,13 +63,11 @@ def replaceSpecial(line):
     line = line.replace('&quot;', '"')
     line = line.replace('&#10;', '')
     
-    
     alphanumeric = string.ascii_lowercase + '0123456789' + '-' + ' ' + '_' 
     line = re.sub(r'[^\w\s]',' ',line)
     line = re.sub(r'\b\w{1,2}\b','',line)
     return line
     
-
 def getText(line,tag):
     # Gets the text obtained between the given tag <tag></tag>
     sTag = '<' + tag +'>'
@@ -79,40 +76,29 @@ def getText(line,tag):
     endIndex = line.find(eTag)  
     text = line[startIndex:endIndex]
     return text
-    
 
 #Creates the text files   
 def termsTxt(tag,line,rowID,terms):
     line = replaceSpecial(line)
-
     line = line.split(" ")
     for word in line:
         if len(word) != 0 and len(word)>2:
             terms.write(tag + "-"+  word.lower() + ":" + rowID + "\n")
 
-
-
 def emailsTxt(fld,em,rowID,emails):
-
     em = em.split(",")
     for e in em:
         if len(e) != 0:
             emails.write(fld + "-"+  e.lower() + ":" + rowID + "\n")
 
-
-
 def dateTxt(da,rowID,dates):
-
     if len(da) != 0:
         dates.write(da + ":" + rowID +"\n")
 
 
 def recsTxt(rowID,rec,records):
-
     if len(rec) != 0:
         records.write(rowID + ":" + rec +"\n")
-
-
     
 if __name__ == '__main__':
     main()
