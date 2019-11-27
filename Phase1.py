794067eb86fa31df1d72c0b2968370a3ab70d1a3
9614 <46434108+9614@users.noreply.github.com>
2019-11-27 15:44:20 -0700

    Add files via upload

--- Phase1.py
+++ Phase1.py
@@ -1,5 +1,6 @@
 import time
 import string
+import re
 
 def main():
     valid = False
@@ -11,7 +12,13 @@ def main():
             print('Invalid file name. Try again.\n')
         else:
             valid = True
-
+    
+    terms = open("terms.txt",'a')
+    emails = open("emails.txt",'a')
+    dates = open("dates.txt",'a')
+    records = open("recs.txt",'a')
+    
+    
     #Starts the timer
     start =time.time()
     line = file.readline()
@@ -26,17 +33,21 @@ def main():
             bcc = getText(line,'bcc')
             date = getText(line,'date')
             
-            termsTxt("s", subj, rowID)
-            termsTxt("b", body, rowID)
-            emailsTxt("from", from1, rowID)
-            emailsTxt("to", to, rowID)
-            emailsTxt("cc", cc, rowID)
-            emailsTxt("bcc", bcc, rowID)
-            dateTxt(date,rowID)
-            recsTxt(rowID,line)
+            termsTxt("s", subj, rowID,terms)
+            termsTxt("b", body, rowID,terms)
+            emailsTxt("from", from1, rowID,emails)
+            emailsTxt("to", to, rowID,emails)
+            emailsTxt("cc", cc, rowID,emails)
+            emailsTxt("bcc", bcc, rowID,emails)
+            dateTxt(date,rowID,dates)
+            recsTxt(rowID,line,records)
         line = file.readline()
         
     file.close()
+    terms.close()
+    emails.close()
+    dates.close()
+    records.close()
     
     #Stops the timer and prints time
     end=time.time()
@@ -55,9 +66,8 @@ def replaceSpecial(line):
     
     
     alphanumeric = string.ascii_lowercase + '0123456789' + '-' + ' ' + '_' 
-    for letter in line:
-        if letter.lower() not in alphanumeric:
-            line = line.replace(letter, ' ')
+    line = re.sub(r'[^\w\s]',' ',line)
+    line = re.sub(r'\b\w{1,2}\b','',line)
     return line
     
 
@@ -72,36 +82,36 @@ def getText(line,tag):
     
 
 #Creates the text files   
-def termsTxt(tag,line,rowID):
+def termsTxt(tag,line,rowID,terms):
     line = replaceSpecial(line)
-    terms = open("terms.txt",'a')
+
     line = line.split(" ")
     for word in line:
         if len(word) != 0 and len(word)>2:
             terms.write(tag + "-"+  word.lower() + ":" + rowID + "\n")
-    terms.close()
 
 
-def emailsTxt(fld,email,rowID):
-    emails = open("emails.txt",'a')
-    email = email.split(",")
-    for e in email:
+
+def emailsTxt(fld,em,rowID,emails):
+
+    em = em.split(",")
+    for e in em:
         if len(e) != 0:
             emails.write(fld + "-"+  e.lower() + ":" + rowID + "\n")
-    emails.close()
 
 
-def dateTxt(date,rowID):
-    dates = open("dates.txt",'a')
-    if len(date) != 0:
-        dates.write(date + ":" + rowID +"\n")
-    dates.close()
 
-def recsTxt(rowID,rec):
-    recs = open("recs.txt",'a')
+def dateTxt(da,rowID,dates):
+
+    if len(da) != 0:
+        dates.write(da + ":" + rowID +"\n")
+
+
+def recsTxt(rowID,rec,records):
+
     if len(rec) != 0:
-        recs.write(rowID + ":" + rec +"\n")
-    recs.close()    
+        records.write(rowID + ":" + rec +"\n")
+
 
     
 if __name__ == '__main__':
